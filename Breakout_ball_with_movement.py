ede40626331a73218da9492ba80c0b49acb466a6
EmanoJcsh <65429505+EmanoelCardosoJr@users.noreply.github.com>
2022-02-18 13:05:53 -0400

    Bola com funções de movimento
    Foram adicionadas duas funções para um constante aumento de velocidade

--- Breakout_ball_with_movement.py
+++ Breakout_ball_with_movement.py
@@ -5,6 +5,7 @@ BLACK = (0, 0, 0)
 
 
 class Ball(pygame.sprite.Sprite):
+    
     # This class represents a ball. It derives from the "Sprite" class in Pygame.
 
     def __init__(self, color, width, height):
